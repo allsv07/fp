8e26a69d786a12fcccd58838a11290ae83bb3cb6
Your Name <test@example.com>
2021-04-01 23:44:54 +0300

    get user by date

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 require_once('db.php');
 
-//get user
-$result = $db->query("SELECT * FROM `users` WHERE YEAR(`bdate`) = '1990'");
+if (isset($_POST['btn_birth'])) {
+    $date = htmlspecialchars($_POST['date_birth']);
+
+    //get user
+    $result = $db->prepare("SELECT * FROM `users` WHERE YEAR(`bdate`) = ?");
+    $result->execute([$date]);
+} else {
+    $result = $db->query("SELECT * FROM `users`");
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,36 +19,18 @@ $result = $db->query("SELECT * FROM `users` WHERE YEAR(`bdate`) = '1990'");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+
     <title>user</title>
 </head>
 <body>
-<style>
-    table {
-        border: 1px solid #eee;
-        table-layout: fixed;
-        width: 100%;
-        margin-bottom: 20px;
-    }
-
-    table th {
-        font-weight: bold;
-        padding: 5px;
-        background: #efefef;
-        border: 1px solid #ddd;
-    }
-
-    table td {
-        padding: 5px 10px;
-        border: 1px solid #eee;
-        text-align: left;
-    }
-</style>
 
 <table>
     <th>№</th>
     <th>First Name</th>
     <th>Last Name</th>
     <th>Date of birth</th>
+    <? if (isset($result)): ?>
     <? while ($user = $result->fetch(PDO::FETCH_ASSOC)): ?>
         <tr>
             <td><?= $user['id'] ?></td>
@@ -49,6 +39,17 @@ $result = $db->query("SELECT * FROM `users` WHERE YEAR(`bdate`) = '1990'");
             <td><?= $user['bdate'] ?></td>
         </tr>
     <? endwhile; ?>
+    <? endif; ?>
 </table>
+
+<div class="block-form">
+    <form action="" method="post" class="form_birth">
+        <label class="label_birth_date" for="date">Введите год</label>
+        <input type="text" name="date_birth" id="date_birth" class="date_birth">
+        <input type="submit" name="btn_birth" class="btn_birth" value="Показать">
+    </form>
+</div>
+
+
 </body>
 </html>
