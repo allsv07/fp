8c1e6115fa7cc64041bd7a74e2d42054346b0711
Your Name <test@example.com>
2021-04-03 15:08:59 +0300

    get user whith ajax js

--- ajax/user.php
+++ ajax/user.php
@@ -2,18 +2,11 @@
 require_once('../db.php');
 require_once('../libs/function.php');
 
-if (isset($_POST['date_birth']) && !empty($_POST['date_birth'])) {
-    $date = htmlspecialchars($_POST['date_birth']);
-    $result = $db->prepare("SELECT * FROM `users` WHERE YEAR(`bdate`) = ?");
-    $result->execute([$date]);
-    $arrUsers = $result->fetchAll(PDO::FETCH_ASSOC);
+$data = json_decode(file_get_contents('php://input'));
+$data = htmlspecialchars($data);
 
-    foreach ($arrUsers as $user) { ?>
-        <tr>
-            <td><?=$user['id']?></td>
-            <td><?=$user['first_name']?></td>
-            <td><?=$user['last_name']?></td>
-            <td><?=$user['bdate']?></td>
-        </tr>
-    <?php }
-}
+$result = $db->prepare("SELECT * FROM `users` WHERE YEAR(`bdate`) = ?");
+$result->execute([$data]);
+$arrUsers = $result->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($arrUsers);
--- libs/script.js
+++ libs/script.js
@@ -1,20 +1,33 @@
-$(document).ready(function () {
-    let year = $("#date_birth").val();
-    let button = $("#btn_birth");
+window.onload = function () {
+    let button = document.querySelector("#btn_birth");
 
-    button.on('click', function (e) {
-        e.preventDefault();
+    button.onclick = function (evt) {
+        evt.preventDefault();
+        let year = document.querySelector("#date_birth").value;
 
-        $.ajax({
-            type: "POST",
-            url: '../ajax/user.php',
-            data: {date_birth: $("#date_birth").val()},
-            success: function (data) {
-                $("tbody").html(data);
-                $(".count_user > strong").html($("tbody").find('tr').length);
+        /*** функция Запроса на сервер ***/
+        async function postData(url = '', data) {
+            const response = await fetch(url, {
+                method: 'POST',
+                body: data
+            });
+            return await response.json();
+        }
+
+        postData('../ajax/user.php', year).then(data => {
+            document.querySelector(".count_user > strong").innerText = data.length;
+            let list = '';
+
+            for (let i = 0; i < data.length; i++) {
+                list += '<tr>' +
+                '<td>'+data[i]['id']+'</td>' +
+                '<td>'+data[i]['first_name']+'</td>' +
+                '<td>'+data[i]['last_name']+'</td>' +
+                '<td>'+data[i]['bdate']+'</td>' +
+                '</tr>';
             }
-        });
 
-    });
-
-});
+            document.querySelector("tbody").innerHTML = list;
+        })
+    }
+};
